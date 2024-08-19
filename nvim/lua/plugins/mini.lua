bdaed4fa4dbfb4fb7941756450a62abc75c6762d
iofq <cjriddz@protonmail.com>
2024-08-19 02:00:41 -0500

    race condition breaks statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,7 @@
 return {
   {
     'echasnovski/mini.nvim',
-    lazy = false,
+    event = "VeryLazy",
     config = function()
       require('mini.ai').setup()
       require('mini.comment').setup()
